b6b4cb8ca106008474bdfd3415589888d9de5c22
griffar2 <andy_griff@live.com>
2014-08-12 23:30:30 +0000

    Lab 4.1 working smoothly

--- splatter/app/models/user.rb
+++ splatter/app/models/user.rb
@@ -1,20 +1,6 @@
 class User < ActiveRecord::Base
 has_many :splatts
 
-validates :name, presence: true
-
-validates :email, uniqueness: {case_sensitive: false}
-
-validates :password, length: {minimum: 8}, if::strong?
-
-def strong?
-	password =~ /.*\d+.*/ && \
-	password =~ /.*[a-z]+.*/ && \
-	password =~ /.*[A-Z].*/
-end
-
-
-
 has_and_belongs_to_many :follows,
 	class_name: "User",
 	join_table: :follows,
@@ -26,4 +12,17 @@ has_and_belongs_to_many :followed_by,
 	join_table: :follows,
 	foreign_key: :followed_id,
 	association_foreign_key: :follower_id	
+	
+validates :name, presence: true
+
+validates :email, uniqueness: {case_sensitive: false}
+
+validates :password, length: {minimum: 8}#, if: :strong?
+
+#def strong?
+#	password =~ /.*\d+.*/ && \
+#	password =~ /.*[a-z]+.*/ && \
+#	password =~ /.*[A-Z].*/
+#end
+
 end
