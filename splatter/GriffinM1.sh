a5a72fad5b35568c48f2f8b431d7bbaf93ecf2da
griffar2 <andy_griff@live.com>
2014-08-13 07:16:27 +0000

    Code cleaned up

---
+++ splatter/GriffinM1.sh
@@ -0,0 +1,50 @@
+### Andrew Griffin Milestone 1 shell script ###
+
+#Create 3 Users
+
+curl -i -H "Content-type: application/json" -X POST http://griffin.sqrawler.com:3000/users -d '{"user": {"email":"andrew@student.op.ac.nz", "name":"Andrew Grififn", "password":"123Tester"}}'
+curl -i -H "Content-type: application/json" -X POST http://griffin.sqrawler.com:3000/users -d '{"user": {"email":"liam@student.op.ac.nz", "name":"Liam Donald", "password":"Tester123"}}'
+curl -i -H "Content-type: application/json" -X POST http://griffin.sqrawler.com:3000/users -d '{"user": {"email":"alex@student.op.ac.nz", "name":"Alex Jones", "password":"Testing55"}}'
+
+#Create 5 splatts for each user, mixed order
+
+curl -i -H "Content-type: application/json" -X POST http://griffin.sqrawler.com:3000/splatts -d '{"splatt": {"body":"My first splatt!!", "user_id":1}}'
+curl -i -H "Content-type: application/json" -X POST http://griffin.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Splatt number 1!", "user_id":3}}'
+curl -i -H "Content-type: application/json" -X POST http://griffin.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Alexs second splatt", "user_id":3}}'
+curl -i -H "Content-type: application/json" -X POST http://griffin.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Splatt number two", "user_id":1}}'
+curl -i -H "Content-type: application/json" -X POST http://griffin.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Third splatt", "user_id":1}}'
+curl -i -H "Content-type: application/json" -X POST http://griffin.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Liam splatting for the first time", "user_id":2}}'
+curl -i -H "Content-type: application/json" -X POST http://griffin.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Splatt splatt", "user_id":2}}'
+curl -i -H "Content-type: application/json" -X POST http://griffin.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Alex splatting", "user_id":3}}'
+curl -i -H "Content-type: application/json" -X POST http://griffin.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Having another splatt", "user_id":1}}'
+curl -i -H "Content-type: application/json" -X POST http://griffin.sqrawler.com:3000/splatts -d '{"splatt": {"body":"One last splatt", "user_id":1}}'
+curl -i -H "Content-type: application/json" -X POST http://griffin.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Splatt time!", "user_id":3}}'
+curl -i -H "Content-type: application/json" -X POST http://griffin.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Having a splatt", "user_id":2}}'
+curl -i -H "Content-type: application/json" -X POST http://griffin.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Fourth splatt", "user_id":2}}'
+curl -i -H "Content-type: application/json" -X POST http://griffin.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Fith Splatt", "user_id":2}}'
+curl -i -H "Content-type: application/json" -X POST http://griffin.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Last Splatt!", "user_id":3}}'
+
+#Causes the first user to follow the other 2
+
+curl -i -H "Content-type: application/json" -X POST http://griffin.sqrawler.com:3000/users/follows -d '{"id":1, "follows_id":2}'
+curl -i -H "Content-type: application/json" -X POST http://griffin.sqrawler.com:3000/users/follows -d '{"id":1, "follows_id":3}'
+
+#Gets the first user’s splatts
+
+curl -i -H "Content-type: application/json" -X GET http://griffin.sqrawler.com:3000/users/splatts/1
+
+#Gets the users followed by the first user
+
+curl -i -H "Content-type: application/json" -X GET http://griffin.sqrawler.com:3000/users/followers/1
+
+#gets the first user’s news feed
+
+curl -i -H "Content-type: application/json" -X GET http://griffin.sqrawler.com:3000/users/splatts-feed/1
+
+#causes the first users to unfollow the third user
+
+curl -i -H "Content-type: application/json" -X DELETE http://griffin.sqrawler.com:3000/users/follows/1/3
+
+#displays the first user’s news feed after unfollowing the third
+
+curl -i -H "Content-type: application/json" -X GET http://griffin.sqrawler.com:3000/users/splatts-feed/1
--- splatter/app/controllers/users_controller.rb
+++ splatter/app/controllers/users_controller.rb
@@ -48,52 +48,63 @@ class UsersController < ApplicationController
     head :no_content
   end
   
-	def splatts
-		@user = User.find(params[:id])
-		
-		render json: @user.splatts
-	end
-	
-	def show_follows
-		@user = User.find(params[:id])
-		
-		render json: @user.follows
-	end
-	
-	def show_followers
-		@user = User.find(params[:id])
-		
-		render json: @user.followed_by
-	end
-	
-	def add_follows
-		@follower = User.find(params[:id])
-		@followed = User.find(params[:follows_id])
-		
-		if @follower.follows << @followed
-			
-		else
-			render json @follower.errors
-		end
-	end
-	
-	def delete_follows
-		@follower = User.find(params[:id])
-		@followed = User.find(params[:follows_id])
-		
-		if @follower.follows.delete(@followed)
-			
-		else
-			render json @follower.errors
-		end
-	end
-	
-	# GET /users/splatts-feed/1
-	def splatts_feed
-		@feed = Splatt.find_by_sql("SELECT user_id, body, created_at FROM splatts JOIN follows ON follows.followed_id = splatts.user_id WHERE follows.follower_id = '#{params[:id]}' ORDER BY splatts.created_at")
-		
-		render json: @feed
-	end
+  # GET /users/splatts/1
+  # GET /users/splatts/1.json  
+  def splatts
+  	@user = User.find(params[:id])
+  	
+  	render json: @user.splatts
+  end
+  
+  # GET /users/follows/1
+  # GET /users/follows/1.json
+  def show_follows
+  	@user = User.find(params[:id])
+  	
+  	render json: @user.follows
+  end
+  
+  # GET /users/followers/1
+  # GET /users/followers/1.json
+  def show_followers
+  	@user = User.find(params[:id])
+  	
+  	render json: @user.followed_by
+  end
+  
+  # POST /users/follows
+  # POST /users/follows.json
+  def add_follows
+  	@follower = User.find(params[:id])
+  	@followed = User.find(params[:follows_id])
+  	
+  	if @follower.follows << @followed
+  		
+  	else
+  		render json @follower.errors
+  	end
+  end
+  
+  # DELETE /users/follows/1/2
+  # DELETE /users/follows/1/2.json
+  def delete_follows
+  	@follower = User.find(params[:id])
+  	@followed = User.find(params[:follows_id])
+  	
+  	if @follower.follows.delete(@followed)
+  		
+  	else
+  		render json @follower.errors
+  	end
+  end
+  
+  # GET /users/splatts-feed/1
+  # GET /users/splatts-feed/1.json
+  def splatts_feed
+  	@feed = Splatt.find_by_sql("SELECT user_id, body, created_at FROM splatts JOIN follows ON follows.followed_id = splatts.user_id WHERE follows.follower_id = '#{params[:id]}' ORDER BY splatts.created_at")
+  	
+  	render json: @feed
+  end
   
 private
 	def user_params(params)
